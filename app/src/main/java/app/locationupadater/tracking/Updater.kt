fa5b24ae2314144ca45d153a848c711b84ae5e5a
dvphuit <61218678+dvphuit@users.noreply.github.com>
2021-03-17 23:51:36 +0700

    FIX POST DATA

--- app/src/main/java/app/locationupadater/tracking/Updater.kt
+++ app/src/main/java/app/locationupadater/tracking/Updater.kt
@@ -3,16 +3,13 @@ package app.locationupadater.tracking
 import android.util.Log
 import android.webkit.WebView
 import app.locationupadater.MyApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.BufferedWriter
-import java.io.IOException
-import java.io.OutputStreamWriter
+import app.locationupadater.Repository
+import kotlinx.coroutines.*
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.math.atan2
@@ -53,13 +50,16 @@ class Updater(private val webView: WebView? = null) {
                     "&" + urlEncodeString("list", mList) +
                     "&" + urlEncodeString("gps", "${lat},${lng}")
 
-            Log.d("TEST", "tracking -> ${MyApp.urlToRequest}, $params")
 
             webView?.evaluateJavascript("javascript:getLocation()") {
                 Log.d("TEST", "call js $it")
             }
             CoroutineScope(Dispatchers.IO).launch {
-                postLocation(params)
+                if (MyApp.user != null && MyApp.urlToRequest != null) {
+                    val result = post(MyApp.urlToRequest!!, params)
+                    Log.d("TEST", "request result: $result")
+                }
+
             }
         }
     }
@@ -98,10 +98,81 @@ class Updater(private val webView: WebView? = null) {
             val wr = OutputStreamWriter(conn.outputStream)
             wr.write(params)
             wr.flush()
+
+
+
+
             mList = ""
         } catch (e: IOException) {
             Log.e("TEST", e.localizedMessage ?: "request error")
         }
+    }
+
+
+    private fun postData(urlStr: String, params: String) {
+        Log.d("TEST", "tracking -> ${urlStr}?$params")
+        val postData: ByteArray = params.toByteArray(StandardCharsets.UTF_8)
+        val postDataLength = postData.size
+        val url = URL(urlStr)
+        val conn = url.openConnection() as HttpURLConnection
+        conn.doOutput = true
+        conn.instanceFollowRedirects = false
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        conn.setRequestProperty("charset", "utf-8")
+        conn.setRequestProperty("Content-Length", postDataLength.toString())
+        conn.useCaches = false
+        DataOutputStream(conn.outputStream).use { wr -> wr.write(postData) }
+        mList = ""
+
+
+    }
+
+    private fun post(urlStr: String, params: String): String? {
+        var response: String? = null
+
+        var conn: HttpURLConnection? = null
+        var dos: DataOutputStream? = null
+        var inputStream: InputStream? = null
+
+        try {
+            val url = URL(urlStr)
+            conn = url.openConnection() as HttpURLConnection
+            conn.doInput = true
+            conn.doOutput = true
+            conn.connectTimeout = 15000
+            conn.requestMethod = "POST"
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+            conn.setRequestProperty("charset", "utf-8")
+            conn.setRequestProperty("Content-Length", params.toByteArray().size.toString())
+
+            dos = DataOutputStream(conn.outputStream)
+            dos.writeBytes(params)
+            dos.flush()
+
+            inputStream = conn.inputStream;
+            val s = Scanner(inputStream).useDelimiter("\\A")
+            response = if (s.hasNext()) s.next() else null
+        } finally {
+            dos?.close()
+            inputStream?.close()
+            conn?.disconnect()
+        }
+
+        return response;
+    }
+
+
+    private fun getQuery(params: List<Pair<String, String>>): String {
+        val result = StringBuilder()
+        var first = true
+        for (pair in params) {
+            if (first) first = false else result.append("&")
+            result.append(URLEncoder.encode(pair.first, "UTF-8"))
+            result.append("=")
+            result.append(URLEncoder.encode(pair.second, "UTF-8"))
+        }
+        return result.toString()
     }
 
 }
